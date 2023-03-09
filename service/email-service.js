efa9b1ef093a127d07da65bb6fc14b205ec53d90
batyanoskow <95103026+batyanoskow@users.noreply.github.com>
2023-03-09 20:33:30 +0200

    Update email-service.js

--- service/email-service.js
+++ service/email-service.js
@@ -1,21 +1,16 @@
 const fs = require("fs");
 const MailService = require('./mail-service');
 class EmailService {
-    async sendMessageToEmails(filePath){
-       
+    async sendMessageToEmails(filePath , html){
         fs.readFile(filePath, async (err, data) => {
             if (err) {
               console.error(err);
               return;
             }
             let splitedData = data.toString().split(",");
-            
             // Вміст файлу міститься в змінній data
             for(let i = 0; i < splitedData.length; i++){
-                
-                 await MailService.sendMail(splitedData[i])
-                
-                 
+                 await MailService.sendMail(splitedData[i] , html)  
             }
             // Видалення тимчасового файлу
             fs.unlink(filePath, (err) => {
@@ -26,6 +21,4 @@ class EmailService {
           });
     }
 }
-
-
 module.exports = new EmailService();
